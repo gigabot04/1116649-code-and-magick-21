8b107ee4d85e62a87b5feff914ece33452e98340
Костя <GigaBOT4@yandex.ru>
2020-09-24 14:46:46 +0300

    маги без повторений имен, фамилий, цвета

--- js/setup.js
+++ js/setup.js
@@ -1,29 +1,30 @@
 'use strict';
 
 document.querySelector(`.setup`).classList.remove(`hidden`);
-const firstName = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
-const secondName = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
+const firstNames = [`Иван`, `Хуан Себастьян`, `Мария`, `Кристоф`, `Виктор`, `Юлия`, `Люпита`, `Вашингтон`];
+const secondNames = [`да Марья`, `Верон`, `Мирабелла`, `Вальц`, `Онопко`, `Топольницкая`, `Нионго`, `Ирвинг`];
 const coatColorWizard = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const eyesColorWizard = [`black`, `red`, `blue`, `yellow`, `green`];
 const similarListElement = document.querySelector(`.setup-similar-list`);
-let wizards = [];
-const template = document.querySelector(`#similar-wizard-template`)
-  .content
-  .querySelector(`.setup-similar-item`);
+const wizards = [];
+const template = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 
 const randNum = (arr) => {
   return Math.floor(Math.random() * arr.length);
 };
 
-for (let i = 0; i < 4; i++) {
-  const wizardTest =
-    {
-      name: firstName[randNum(firstName)] + ` ` + secondName[randNum(secondName)],
-      coatColor: coatColorWizard[randNum(coatColorWizard)],
-      eyesColor: eyesColorWizard[randNum(eyesColorWizard)]
-    };
-  wizards.push(wizardTest);
-}
+const createWizard = () => {
+  const firstName = firstNames.splice(randNum(firstNames), 1).pop();
+  const secondName = secondNames.splice(randNum(secondNames), 1).pop();
+  const coatColor = coatColorWizard.splice(randNum(coatColorWizard), 1).pop();
+  const eyesColor = eyesColorWizard.splice(randNum(eyesColorWizard), 1).pop();
+
+  return {
+    name: `${firstName} ${secondName}`,
+    coatColor,
+    eyesColor
+  };
+};
 
 const renderWizard = (wizard) => {
   const wizardElement = template.cloneNode(true);
@@ -35,7 +36,11 @@ const renderWizard = (wizard) => {
   return wizardElement;
 };
 
-let fragment = document.createDocumentFragment();
+for (let i = 0; i < 4; i++) {
+  wizards.push(createWizard());
+}
+
+const fragment = document.createDocumentFragment();
 for (let i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
