cfcbde4cab8ea624522ee6f6bdf411e9a1a415b7
Костя <GigaBOT4@yandex.ru>
2020-10-01 11:53:24 +0300

    Сделал изменение цвета глаз, мантии, фаербола. Отправка формы

--- js/setup.js
+++ js/setup.js
@@ -92,3 +92,49 @@ setupClose.addEventListener(`keydown`, function (evt) {
     closePopup();
   }
 });
+
+// Изменение цвета мантии
+
+
+const onCoatColorClick = document.querySelector(`.wizard-coat`);
+let setupCoatColors = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+
+onCoatColorClick.addEventListener(`click`, () => {
+  let inputCoatColor = document.querySelector(`input[name="coat-color"]`);
+  if (setupCoatColors[0] && !setupCoatColors[1]) {
+    setupCoatColors = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
+  }
+  const setupCoatColorDel = setupCoatColors.splice(randNum(setupCoatColors), 1).pop();
+  onCoatColorClick.style.fill = setupCoatColorDel;
+  inputCoatColor.value = setupCoatColorDel;
+});
+
+// Изменение цвета глаз
+
+const onCoatEyeClick = document.querySelector(`.wizard-eyes`);
+let setupEyeColors = [`black`, `red`, `blue`, `yellow`, `green`];
+
+onCoatEyeClick.addEventListener(`click`, () => {
+  let inputEyeColor = document.querySelector(`input[name="eyes-color"]`);
+  if (setupEyeColors[0] && !setupEyeColors[1]) {
+    setupEyeColors = [`black`, `red`, `blue`, `yellow`, `green`];
+  }
+  const setupEyeColorDel = setupEyeColors.splice(randNum(setupEyeColors), 1).pop();
+  onCoatEyeClick.style.fill = setupEyeColorDel;
+  inputEyeColor.value = setupEyeColorDel;
+});
+
+// Изменение цвета фаербола
+
+const onCoatFireballClick = document.querySelector(`.setup-fireball-wrap`);
+let setupFireballColors = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
+
+onCoatFireballClick.addEventListener(`click`, () => {
+  let inputFireballColor = onCoatFireballClick.querySelector(`input[name="fireball-color"]`);
+  if (setupFireballColors[0] && !setupFireballColors[1]) {
+    setupFireballColors = [`#ee4830`, `#30a8ee`, `#5ce6c0`, `#e848d5`, `#e6e848`];
+  }
+  const setupFireballColorDel = setupFireballColors.splice(randNum(setupFireballColors), 1).pop();
+  onCoatFireballClick.style.backgroundColor = setupFireballColorDel;
+  inputFireballColor.value = setupFireballColorDel;
+});
